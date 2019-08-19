8d4fb34459b7bf93d36d48bd526a3d687770d645
TheLessonHere <48539448+TheLessonHere@users.noreply.github.com>
2019-08-19 13:52:59 -0600

    Initial commit with snapshot test added

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,24 @@
 // Test away!
+import React from "react";
+import renderer from "react-test-renderer";
+import { render, fireEvent } from "@testing-library/react";
+import Display from "./Display";
+
+describe("<Display />", () => {
+  it("matches snapshot", () => {
+    const tree = renderer.create(<Display />);
+
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+
+  // it("displays a message when 'speak' button is clicked", () => {
+  //   const speak = jest.fn();
+  //   let messageMock = "";
+
+  //   const { getByText } = render(<Display speak={speak} message={messageMock} />);
+
+  //   fireEvent.click(getByText(/speak/i));
+
+  //   expect(speak).toHaveBeenCalled();
+  // });
+});
